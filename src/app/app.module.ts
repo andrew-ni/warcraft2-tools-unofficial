a3b41d363677e40c29cb5d27e193238fc2cb3b3f
blhough <benhuff322@gmail.com>
2017-10-11 17:47:51 -0700

    Add map class, tile class, and map service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Services
+import { MapService } from 'services/map.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    MapService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
