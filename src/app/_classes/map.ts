dfffb545b85f1e25c9b70cad2f7054a522f4e269
blhough <blhough@ucdavis.edu>
2017-10-11 22:18:16 -0700

    Add some starter code for parsing the map data

--- src/app/_classes/map.ts
+++ src/app/_classes/map.ts
@@ -1,2 +1,11 @@
 export class Map {
+
+  // `mapData` is the raw file contents
+  constructor(mapData: string) {
+    this.parseMapData(mapData);
+  }
+
+  private parseMapData(mapData: string): void {
+
+  }
 }
--- src/app/_services/map.service.ts
+++ src/app/_services/map.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ipcRenderer } from 'electron';
+
+import { Map } from 'map';
 
 @Injectable()
 export class MapService {
+  private _map: Map;
 
-  constructor() { }
+  constructor() {
+    // Event listener for when a map has been loaded from a file.
+    // `mapData` is the raw file contents
+    ipcRenderer.on('map:loaded', (event, mapData) => {
+      this._map = new Map(mapData);
+    });
+  }
 
+  get map(): Map {
+    return this._map;
+  }
 }
