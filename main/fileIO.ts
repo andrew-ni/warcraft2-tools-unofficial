472ec31595e5e0e7f79212f4b0b179e5f865dbd4
basedgiraffe <rifknai@gmail.com>
2017-10-13 15:14:02 -0700

    loading, save, saveas in progress

--- main/fileIO.ts
+++ main/fileIO.ts
@@ -1,17 +1,18 @@
 import { dialog } from 'electron';
 import * as fs from 'fs';
 
+const options = {
+  filters: [
+    { name: 'Map File', extensions: ['map'] }
+  ]
+};
+
 // Module is like a namespace so we don't polute the global space
 export module IO {
   // Note it is async, opening files take a long time and we don't want to block the thread
   export function loadMap(window: Electron.WebContents) {
     console.log('loadMap');
 
-    const options = {
-      filters: [
-        { name: 'Map File', extensions: ['map'] }
-      ]
-    };
 
     // showOpenDialog is Electron
     // This will open a dialog for the user
@@ -20,32 +21,30 @@ export module IO {
       if (paths === undefined) return;
 
       console.log(paths[0]);
-
       // fs.readFile is Node
       // We only handle opening a single map file so we only look at `path[0]`
       fs.readFile(paths[0], 'utf8', (err: Error, data: string) => {
         if (err) {
           console.log(err);
         } else {
-          window.send('map:loaded', data);
+          window.send('map:loaded', data, paths[0]);
         }
       });
     });
   }
+
+  /*
+  TODO: make main thread stateless, have mapservice decide whether a save is valid
+        also handle errors
+  */
+
+  // Renderer allows us to save
+  export function saveMap(window: Electron.WebContents, data: string, filepath: string) {
+    console.log('saveMap');
+
+    if (filepath === undefined) {
+      dialog.showSaveDialog(options, (path: string) => filepath = path);
+    }
+    fs.writeFile(filepath, data, () => {});
+  }
 }
-
-
-// Note: in menubar.ts, it is not necessary for all the items to be bound to ipc messages eg. window.send()
-/*
-
-something like this should work
-
-{
-  label: 'Load Map',
-  click() {
-    IO.loadMap(window);
-   }
-},
-
-
-*/
--- main/menubar.ts
+++ main/menubar.ts
@@ -18,7 +18,9 @@ export function buildMenu(window: Electron.WebContents): void {
         },
         {
           label: 'Save Map',
-          click() { window.send('menu:file:save'); }
+          click() {
+            window.send('menu:file:save');
+        }
         },
         {
           label: 'Save Map As...',
