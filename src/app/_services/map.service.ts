14b49410f9f4daeba6f40609855c6c514cc11c00
blhough <blhough@ucdavis.edu>
2017-10-14 11:55:28 -0700

    Add filePath to map.service

--- src/app/_services/map.service.ts
+++ src/app/_services/map.service.ts
@@ -6,11 +6,13 @@ import { Map } from 'map';
 @Injectable()
 export class MapService {
   private _map: Map;
+  private _filePath: string;
 
   constructor() {
     // Event listener for when a map has been loaded from a file.
     // `mapData` is the raw file contents
-    ipcRenderer.on('map:loaded', (event, mapData) => {
+    ipcRenderer.on('map:loaded', (event, mapData, filePath) => {
+      this._filePath = filePath;
       this._map = new Map(mapData);
     });
   }
