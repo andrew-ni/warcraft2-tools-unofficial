0ca5a97071a5a174cce8cae266ab914288aa2b5a
blhough <blhough@ucdavis.edu>
2017-10-14 13:29:30 -0700

    Inject the MapService so it is actually constructed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapService } from 'services/map.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ECS160 Tools';
+
+  constructor(
+    mapService: MapService
+  ) { }
 }
