fc6ca928b2f6afc29a48cbfd0c165d199a2167ee
andrewni <ajni@ucdavis.edu>
2017-10-21 19:26:59 -0700

    Fixed small merge conflict code

--- src/app/map/tile/tile.component.ts
+++ src/app/map/tile/tile.component.ts
@@ -23,13 +23,7 @@ export class TileComponent implements OnInit {
       case TileType.DarkDirt: return 'assets/tiles/DarkDirt.png';
       case TileType.LightDirt: return 'assets/tiles/LightDirt.png';
       case TileType.Rock: return 'assets/tiles/Rock.png';
-<<<<<<< HEAD
-      // case TileType.RockPartial: return 'assets/tiles/RockPartial.png';
       case TileType.Forest: return 'assets/tiles/Forest.png';
-      // case TileType.ForestPartial: return 'assets/tiles/ForestPartial.png';
-=======
-      case TileType.Forest: return 'assets/tiles/Forest.png';
->>>>>>> c3311feef246a47acae6f576a22354db6a876206
       case TileType.DeepWater: return 'assets/tiles/DeepWater.png';
       case TileType.ShallowWater: return 'assets/tiles/ShallowWater.png';
 
