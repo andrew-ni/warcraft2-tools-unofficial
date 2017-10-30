42c893c41a0fcb3ba191363f2d8e47303060d237
Andy Yang <andytheyang@gmail.com>
2017-10-30 15:31:30 -0700

    numPlayers changing implemented

--- src/app/newmap/newmap.component.ts
+++ src/app/newmap/newmap.component.ts
@@ -15,29 +15,38 @@ export class NewmapComponent implements OnInit {
 
   ngOnInit() {
     this.allPlayerInfo = [{'id': 0, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 1, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 2, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 3, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 4, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 5, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 6, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 7, 'gold': 0, 'lumber': 0, 'assets': []},
-                          {'id': 8, 'gold': 0, 'lumber': 0, 'assets': []}];
-    this.currentPlayers = this.allPlayerInfo.slice(1, 9);
+    {'id': 1, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 2, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 3, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 4, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 5, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 6, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 7, 'gold': 0, 'lumber': 0, 'assets': []},
+    {'id': 8, 'gold': 0, 'lumber': 0, 'assets': []}];
 
-    const button: HTMLButtonElement = document.getElementById('CreateMapButton') as HTMLButtonElement;
-
-    button.addEventListener('click', (event) => {
-      this.buildMap();
-    });
+    this.updateNumPlayers(2);
+    this.setListeners();
   }
 
   fun_close(){
     document.getElementById('newMapModal').setAttribute('style', 'display: none;');
   }
 
+  private setListeners(): void {
+    const createButton: HTMLButtonElement = document.getElementById('CreateMapButton') as HTMLButtonElement;
+    createButton.addEventListener('click', () => this.buildMap());
+
+    const numPlayersField: HTMLSelectElement = document.getElementById('NumPlayersField') as HTMLSelectElement;
+    numPlayersField.addEventListener('click', () => this.updateNumPlayers(Number(numPlayersField.value)));
+  }
+
   private buildMap(): void {
     console.log('buildmap');
+  }
+
+  private updateNumPlayers(num: number): void {
+    console.log(num);
+    this.currentPlayers = this.allPlayerInfo.slice(1, num + 1);   // get whole range
   }
 }
 
