7faf1703869add48101a01fa2e9c31eb13e423c4
Andy Yang <andytheyang@gmail.com>
2017-10-30 14:56:46 -0700

    New Map improvements. Button listener, dependent player fields

--- src/app/newmap/newmap.component.ts
+++ src/app/newmap/newmap.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from 'services/map.service';
+import { Player } from 'player';
 
 @Component({
   selector: 'app-newmap',
@@ -6,17 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newmap.component.scss']
 })
 export class NewmapComponent implements OnInit {
+  public allPlayerInfo: Player[];
+  public currentPlayers: Player[];
 
-  constructor() { }
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
+    this.allPlayerInfo = [{'id': 0, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 1, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 2, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 3, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 4, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 5, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 6, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 7, 'gold': 0, 'lumber': 0, 'assets': []},
+                          {'id': 8, 'gold': 0, 'lumber': 0, 'assets': []}];
+    this.currentPlayers = this.allPlayerInfo.slice(1, 9);
+
+    const button: HTMLButtonElement = document.getElementById('CreateMapButton') as HTMLButtonElement;
+
+    button.addEventListener('click', (event) => {
+      this.buildMap();
+    });
   }
 
   fun_close(){
-    document.getElementById('newMapModal').setAttribute("style", "display: none;");
+    document.getElementById('newMapModal').setAttribute('style', 'display: none;');
+  }
+
+  private buildMap(): void {
+    console.log('buildmap');
   }
 }
 
 require('electron').ipcRenderer.on('menu:file:new', () => {
-  document.getElementById('newMapModal').setAttribute("style", "display: inline;");
-})
+  document.getElementById('newMapModal').setAttribute('style', 'display: inline;');
+});
