da62f588b15da54466d369b47a86fb84286e8d9c
blhough <blhough@ucdavis.edu>
2017-11-02 22:23:09 -0700

    Comment MapService

--- src/app/_services/map.service.ts
+++ src/app/_services/map.service.ts
@@ -9,27 +9,62 @@ import { Tileset } from 'tileset';
 
 @Injectable()
 export class MapService {
+  /** True if the map is in a valid state to save, False otherwise. */
   public canSave = false;
+
+  /** The name of the map. */
   public name: string;
+
+  /** The description for the map. */
   public description: string;
+
+  /** The width of the map in tiles. */
   public width: number;
+
+  /** The height of the map in tiles. */
   public height: number;
+
+  /** A raw matrix of terrain tile types. This is what is saved and loaded. */
   public terrainLayer: TileType[][];
-  public assetLayer: Asset[][];
-  public drawLayer: Tile[][];
-  public partialBits: Uint8Array[];
-  public players: Player[] = [];
+
+  /** A list of assets. */
   public assets: Asset[] = [];
+
+  /**
+   * A matrix that tracks the location of assets in the map.
+   * Assets larger than 1x1 will be assigned to multiple cells.
+   * Shares the sames assets as in the list.
+   */
+  public assetLayer: Asset[][];
+
+  /** A matrix of tiles that is drawn to the canvas. */
+  public drawLayer: Tile[][];
+
+  /** Used in the calculation of the tile index */
+  public partialBits: Uint8Array[];
+
+  /** A list of players */
+  public players: Player[] = [];
+
+  /** The version of map file. */
   public mapVersion: string;
+
+  /** The relative path to the Terrain.dat file. */
   public terrainPath: string;
+
+  /** The tile set used to draw the map. */
   public tileSet: Tileset;
 
 
-  /**
-   * Events
-   */
+  // Events
+
+  /** @event mapResized When the dimension of the map have changed. */
   public mapResized = new Subject<Dimension>();
+
+  /** @event mapLoaded When the assets and terrain have fully been parsed and initialized. */
   public mapLoaded = new Subject<void>();
+
+  /** @event tilesUpdated When the any tile types/indices have changed. */
   public tilesUpdated = new Subject<Region>();
 
   constructor() { }
