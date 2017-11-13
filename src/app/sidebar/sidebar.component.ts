561746a33b8033c3d358f6f48b33aef1d087168f
Kevin Pham <phamkevinx@gmail.com>
2017-11-13 12:53:57 -0800

    add placeholder icons for select & paint cursors

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'services/user.service';
 
+import { AssetType } from 'asset';
+
+interface CursorButton {
+  name: string;
+  imgSrc: string;
+}
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+
+  cursorButtons: CursorButton[] = [
+    {
+      name: 'Select',
+      imgSrc: './assets/frontend_icons/select_tool.png',
+    },
+    {
+      name: 'Paint',
+      imgSrc: './assets/frontend_icons/paint_tool.png',
+    },
+  ];
 
   constructor(private userService: UserService) { }
 
