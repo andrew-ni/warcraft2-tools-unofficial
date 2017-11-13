3904d3c00534ebc1d4a921615ab5ab14674f4861
Eric Lam <eritsericlam@gmail.com>
2017-11-13 15:01:12 -0800

    sidebar propertie update

--- src/app/sidebar/assets/assets.component.ts
+++ src/app/sidebar/assets/assets.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'services/user.service';
+import { MapService } from 'services/map.service';
 
 @Component({
   selector: 'app-assets',
@@ -7,12 +8,96 @@ import { UserService } from 'services/user.service';
   styleUrls: ['./assets.component.scss']
 })
 export class AssetsComponent implements OnInit {
+  PlayerNumber = [];
+  SelectPlayer = 1;
+  GoldAmount;
+  LumberAmount;
+  constructor(private mapService: MapService, private userService: UserService) { 
+    this.mapService.mapLoaded.do(() => console.log('map:loaded')).subscribe({
+      next: () => this.setProperties(),
+      error: err => console.error(err),
+    });
+  }
 
-  constructor(
-    private userService: UserService,
-  ) { }
+  onChangeSelectPlayer(newValue) {
+    console.log(newValue);
+    this.userService.selectedPlayer = this.SelectPlayer;
+    this.GoldAmount = this.mapService.players[this.SelectPlayer].gold;
+    this.LumberAmount = this.mapService.players[this.SelectPlayer].lumber;
+    switch(this.mapService.players.length-1) {
+      case 2:
+        this.PlayerNumber = [1, 2];
+        break;
+      case 3:
+        this.PlayerNumber = [1, 2, 3];
+        break;
+      case 4:
+        this.PlayerNumber = [1, 2, 3, 4];
+        break;
+      case 5:
+        this.PlayerNumber = [1, 2, 3, 4, 5];
+        break;
+      case 6:
+        this.PlayerNumber = [1, 2, 3, 4, 5, 6];
+        break;
+      case 7:
+        this.PlayerNumber = [1, 2, 3, 4, 5, 6, 7];
+        break;
+      case 8:
+        this.PlayerNumber = [1, 2, 3, 4, 5, 6, 7, 8];
+        break;
+      default:
+        console.error();
+        break;
+      }
+    // ... do other stuff here ...
+  }
+
+  onChangeGold(newValue) {
+    console.log(newValue);
+    this.mapService.players[this.SelectPlayer].gold = this.GoldAmount;
+    // ... do other stuff here ...
+  }
+
+  onChangeLumber(newValue) {
+    console.log(newValue);
+    this.mapService.players[this.SelectPlayer].lumber = this.LumberAmount;
+    // ... do other stuff here ...
+  }
+
+  public setProperties() {
+    this.GoldAmount = this.mapService.players[this.SelectPlayer].gold;
+    this.LumberAmount = this.mapService.players[this.SelectPlayer].lumber;
+    switch(this.mapService.players.length-1) {
+      case 2:
+        this.PlayerNumber = [1, 2];
+        break;
+      case 3:
+        this.PlayerNumber = [1, 2, 3];
+        break;
+      case 4:
+        this.PlayerNumber = [1, 2, 3, 4];
+        break;
+      case 5:
+        this.PlayerNumber = [1, 2, 3, 4, 5];
+        break;
+      case 6:
+        this.PlayerNumber = [1, 2, 3, 4, 5, 6];
+        break;
+      case 7:
+        this.PlayerNumber = [1, 2, 3, 4, 5, 6, 7];
+        break;
+      case 8:
+        this.PlayerNumber = [1, 2, 3, 4, 5, 6, 7, 8];
+        break;
+      default:
+        console.error();
+        break;
+      }
+  }
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'services/user.service';
+import { MapService } from 'services/map.service';
+import { Player } from 'player';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,7 +10,40 @@ import { UserService } from 'services/user.service';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  MapName;
+  NumPlayers = [2,3,4,5,6,7,8];
+  NumberPlayers = 1;
+
+  constructor(private mapService: MapService) { 
+    this.mapService.mapLoaded.do(() => console.log('map:loaded')).subscribe({
+      next: () => this.setProperties(),
+      error: err => console.error(err),
+    });
+  }
+
+  onChangeName(newValue) {
+    console.log(newValue);
+    this.mapService.name = this.MapName;
+    // ... do other stuff here ...
+  }
+
+  onChangeNumberPlayers(newValue) {
+    console.log(newValue);
+    if (newValue > this.mapService.players.length - 1)
+    {
+      for (var i = this.mapService.players.length; i <= newValue; i++) {
+        console.log(i);
+        this.mapService.players.push(new Player(i, 2000, 150));
+      }
+    }
+    // ... do other stuff here ...
+  }
+
+  public setProperties() {
+    this.MapName = this.mapService.name;
+    this.NumberPlayers = this.mapService.players.length - 1;
+  }
+
 
   ngOnInit() {
   }
