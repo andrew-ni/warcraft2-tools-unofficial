d21d2d30f65d1fcefcd08eea55ad92f900f3f438
Eric Lam <eritsericlam@gmail.com>
2017-11-13 16:42:11 -0800

    added UI for tileset/audio/animation

---
+++ src/app/audio/audio.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio',
+  templateUrl: './audio.component.html',
+  styleUrls: ['./audio.component.scss']
+})
+export class AudioComponent implements OnInit {
+
+  constructor() { }
+
+  fun_close() {
+    document.getElementById('audioModal').setAttribute('style', 'display: none;');
+  }
+
+  SelectedAudio() {
+    document.getElementById('audio1').setAttribute('style', 'background-color: #666; color:#fff;');
+  }
+
+  ngOnInit() {
+  }
+
+}
+
+require('electron').ipcRenderer.on('menu:file:audio', () => {
+  document.getElementById('audioModal').setAttribute('style', 'display: inline;');
+});
---
+++ src/app/tileset/tileset.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TilesetComponent } from './tileset.component';
+
+describe('TilesetComponent', () => {
+  let component: TilesetComponent;
+  let fixture: ComponentFixture<TilesetComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TilesetComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TilesetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
