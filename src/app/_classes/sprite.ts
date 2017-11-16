8b8ae7010ff966eaf7971ca21aef5d316a0ea4f6
blhough <blhough@ucdavis.edu>
2017-11-15 22:23:56 -0800

    WIP create animations and animationSets

--- src/app/_classes/sprite.ts
+++ src/app/_classes/sprite.ts
@@ -7,45 +7,59 @@ export class Sprite {
   private _image: ImageBitmap;
   private _path: string;
   private _index = 0; // Save index of the "inactive" frame for all structures, for drawing.
+  private animations: AnimationSet[] = [];
 
   constructor() { }
 
   /**
    * Reads the .dat files for the desired asset.
    * Saves the filepath to the .png and the correct drawing index.
-   * @param name AssetType to parse (e.g. 'Peasant')
+   * @param assetName AssetType to parse (e.g. 'Peasant')
    */
-  public async readDat(name: string) {
+  public async readDat(assetName: string) {
     return new Promise<void>((resolve, reject) => {
-      readFile('src/assets/img/' + name + '.dat', 'utf8', (err, data) => {
+      readFile('src/assets/img/' + assetName + '.dat', 'utf8', (err, data) => {
         if (err) { console.error(err); reject(err); }
 
         const [, relPath, , frameNames] = data.split(/#.*?\r?\n/);
         this._path = pathJoin('assets/img/', relPath.trim());
 
-        const animations = new Map<string, number[]>();
+        const rawAnimations = new Map<string, SubFrame[]>();
 
         const frameNamesArray = frameNames.trim().split(/\r?\n/);
         for (const [i, framName] of frameNamesArray.entries()) {
 
-          const match = framName.match(/\b(\w+(-\w+)?)(-(\d+))?\b/);
-          const animationName = match[1];
-          const localIndex = (match[4] === undefined) ? 0 : parseInt(match[4], 10);
+          // Ex. `lumber-nw-3`
+          // Group 1. `lumber`
+          // Group 2. `-nw`
+          // Group 3. `nw`
+          // Group 4. `-3`
+          // Group 5. `3`
+          const match = framName.match(/\b([a-z]+)(-([a-z]+))?(-(\d+))?\b/);
 
-          if (!animations.has(animationName)) {
-            animations.set(animationName, []);
+          const animationName = match[1];
+          const subType = match[3];
+          const subIndex = (match[5] === undefined) ? 0 : parseInt(match[5], 10);
+
+          if (!rawAnimations.has(animationName)) {
+            rawAnimations.set(animationName, []);
           }
 
-          const indices = animations.get(animationName);
-          indices.length = (localIndex + 1) > indices.length ? localIndex + 1 : indices.length;
-          indices[localIndex] = i;
+          rawAnimations.get(animationName).push({ subType, subIndex, index: i });
+
 
           if (framName.includes('inactive')) {
             this._index = i;
             break;
           }
         }
-        console.log(name, animations);
+
+        for (const [name, subFrames] of rawAnimations) {
+          const anim = new AnimationSet(name, subFrames);
+          this.animations.push(anim);
+          this.animations[name] = anim;
+        }
+        console.log(assetName, this.animations);
 
         resolve();
       });
@@ -57,5 +71,45 @@ export class Sprite {
   get index() { return this._index; }
 
   set image(img: ImageBitmap) { this._image = img; }
+}
 
+
+interface SubFrame {
+  subType: string;
+  subIndex: number;
+  index: number;
+}
+
+export class Animation {
+  constructor(
+    private name: string,
+    private indices: number[],
+  ) { }
+}
+
+export class AnimationSet {
+  private animations: Animation[] = [];
+
+  constructor(
+    private name: string,
+    frames: SubFrame[]
+  ) {
+    const rawAnimations = new Map<string, number[]>();
+
+    for (const frame of frames) {
+      if (!rawAnimations.has(frame.subType)) {
+        rawAnimations.set(frame.subType, []);
+      }
+
+      const indices = rawAnimations.get(frame.subType);
+      indices.length = (frame.subIndex + 1) > indices.length ? frame.subIndex + 1 : indices.length;
+      indices[frame.subIndex] = frame.index;
+    }
+
+    for (const [animationName, indices] of rawAnimations) {
+      const anim = new Animation(animationName, indices);
+      this.animations.push(anim);
+      this.animations[animationName] = anim;
+    }
+  }
 }
