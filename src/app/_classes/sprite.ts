a283b9d2d2e3e5cb821a3a6e487a425302e3b2d4
Andy Yang <andytheyang@gmail.com>
2017-11-19 13:07:02 -0800

    Added AnimationContext

--- src/app/_classes/sprite.ts
+++ src/app/_classes/sprite.ts
@@ -46,3 +46,69 @@ export class AnimationAction {
   get name() { return this._name; }
   get directions() { return this._directions; }
 }
+
+/**
+ * Represents the state of an animation and its associated Animation objects.
+ */
+export class AnimationContext {
+  /** The action of this current animation. */
+  private action: AnimationAction;
+
+  /** The direction of this current animation. */
+  private direction: AnimationDirection;
+
+  /**
+   * Constructs a new AnimationContext. Useful for changing Sprites.
+   * @param sprite The new sprite this AnimationContext should reflect.
+   * @param frameNum Frame number to initialize to. Default is 0.
+   */
+  constructor(
+    private sprite: Sprite,
+    private frameNum: number = 0,
+  ) {
+    this.action = this.sprite.actions[0];   // default to first actionm
+    this.direction = this.action.directions[0]; // default to first direction
+  }
+
+  /**
+   * Set the action to the index specified.
+   * @param a String or number, index of action within this Sprite to take on.
+   */
+  public setAction(a: string|number): void {
+    this.action = this.sprite.actions[a];
+    this.setDirection(0);
+  }
+
+  /**
+   * Set the driection to the index specified.
+   * @param a String or number, index of direction within this action to take on.
+   */
+  public setDirection(d: string|number): void {
+    this.direction = this.action.directions[d];
+  }
+
+  /** Get the current frame number. Index in animation sprite sheet. */
+  public getCurFrame(): number {
+    return this.frameNum;
+  }
+
+  /** Advance to next frame and return updated frame number. */
+  public nextFrame(): number {
+    this.frameNum++;
+    if (this.frameNum >= this.direction.frames.length) {
+      this.frameNum = 0;
+    }
+
+    return this.getCurFrame();
+  }
+
+  /** Go to previous frame and return updated frame number. */
+  public prevFrame(): number {
+    this.frameNum--;
+    if (this.frameNum < 0) {
+      this.frameNum = this.direction.frames.length - 1;
+    }
+
+    return this.getCurFrame();
+  }
+}
