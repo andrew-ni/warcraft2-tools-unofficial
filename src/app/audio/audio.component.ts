bbb43fb37373995530d4ca46b50fa76d11461110
blhough <benhuff322@gmail.com>
2017-11-21 15:53:37 -0800

    Clean up audio component

--- src/app/audio/audio.component.ts
+++ src/app/audio/audio.component.ts
@@ -7,23 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudioComponent implements OnInit {
 
-  SongCategories = ["BGM", "Peasant", "Footman", "Archer", "Ranger"];
-
   constructor() { }
-
-  fun_close() {
-    document.getElementById('audioModal').setAttribute('style', 'display: none;');
-  }
-
-  SelectedAudio() {
-    document.getElementById('audio1').setAttribute('style', 'background-color: #666; color:#fff;');
-  }
 
   ngOnInit() {
   }
-
 }
-
-require('electron').ipcRenderer.on('menu:file:audio', () => {
-  document.getElementById('audioModal').setAttribute('style', 'display: inline;');
-});
