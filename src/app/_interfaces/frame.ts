a1a57da0c5d789236fba099493b2cf339061993d
Andy Yang <andytheyang@gmail.com>
2017-11-21 17:32:17 -0800

    Removed obsolete Frame

--- src/app/_interfaces/frame.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents the triplet found in each line in asset .dat files.
- * Some lines have no direction and/or index (e.g. "inactive" or "construct-0"), so they are optional.
- */
-export interface Frame {
-  action: string;
-  direction?: string;
-  index?: number;
-}
