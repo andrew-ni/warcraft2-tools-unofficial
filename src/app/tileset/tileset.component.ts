9d24c3e7496e4e7970f4e221e7e018cc3ec6f62f
unknown <kjconley@ucdavis.edu>
2017-11-27 01:37:59 -0800

    implement click registration

--- src/app/tileset/tileset.component.ts
+++ src/app/tileset/tileset.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Coordinate } from 'interfaces';
 import { TilesetService } from 'services/tileset.service';
 
 @Component({
@@ -9,6 +10,8 @@ import { TilesetService } from 'services/tileset.service';
 })
 export class TilesetComponent implements OnInit {
 
+  private eventHandler: HTMLDivElement;
+
   private tilesetCanvas: HTMLCanvasElement;
   private tilesetContext: CanvasRenderingContext2D;
 
@@ -17,9 +20,27 @@ export class TilesetComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.eventHandler = document.getElementById('tilesetevents') as HTMLDivElement;
     this.tilesetCanvas = document.getElementById('tilesetCanvas') as HTMLCanvasElement;
     this.tilesetContext = this.tilesetCanvas.getContext('2d');
     this.tilesetService.setCanvas(this.tilesetCanvas, this.tilesetContext);
+    this.setClickListeners();
   }
+
+  private setClickListeners(){
+    let clickPos: Coordinate;
+
+
+
+
+
+
+  this.eventHandler.addEventListener('click', (event) => {
+
+    clickPos = { x: event.offsetX, y: event.offsetY };
+    console.log(Math.floor(clickPos.x / 64), Math.floor(clickPos.y / 64));
+
+  });
+}
 }
 
