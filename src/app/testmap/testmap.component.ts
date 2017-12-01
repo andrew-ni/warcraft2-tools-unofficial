a0142fc0ef64a99fdd9bffb081ce97f4fdd7a28d
basedgiraffe <rifknai@gmail.com>
2017-11-30 17:47:17 -0800

    click handlers done

--- src/app/testmap/testmap.component.ts
+++ src/app/testmap/testmap.component.ts
@@ -7,8 +7,6 @@ import { Coordinate } from 'interfaces';
   styleUrls: ['./testmap.component.scss']
 })
 export class TestmapComponent implements OnInit {
-  private eventHandler: HTMLDivElement;
-
   private bottomCanvas: HTMLCanvasElement;
   private bottomContext: CanvasRenderingContext2D;
 
@@ -18,7 +16,6 @@ export class TestmapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.eventHandler = document.getElementById('events') as HTMLDivElement;
     this.bottomCanvas = document.getElementById('bottomCanvas') as HTMLCanvasElement;
     this.bottomContext = this.bottomCanvas.getContext('2d');
     this.topCanvas = document.getElementById('topCanvas') as HTMLCanvasElement;
@@ -27,14 +24,11 @@ export class TestmapComponent implements OnInit {
     this.topCanvas.height = 512;
     this.bottomCanvas.width = 512;
     this.bottomCanvas.height = 512;
-    console.log(this.bottomCanvas.height);
 
     this.setClickListeners();
   }
 
   private setClickListeners() {
-    // let clickPos: Coordinate;
-
     // const x = Math.floor(event.offsetX / CanvasService.TERRAIN_SIZE);
     // const y = Math.floor(event.offsetY / CanvasService.TERRAIN_SIZE);
 
@@ -42,8 +36,7 @@ export class TestmapComponent implements OnInit {
      * On mousedown, being pathfinding calculations
      * https://developer.mozilla.org/en-US/docs/Web/Events/mousedown; 0=leftclick, 1=middleclick, 2=rightclick
      */
-    this.eventHandler.addEventListener('mousedown', (event) => {
-      // clickPos = { x: event.offsetX, y: event.offsetY };
+    this.topCanvas.addEventListener('mousedown', (event) => {
       if (event.button === 0) {
         this.pathfind({ x: event.offsetX, y: event.offsetY });
       }
