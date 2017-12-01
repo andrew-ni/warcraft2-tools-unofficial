9eee3cd49a7db9339efaacad474ff5dd5163d216
Daniel Wang <danielwang405@gmail.com>
2017-11-30 21:01:38 -0800

    moved sound to data folder, created listener on new package load to reparse sounds

--- src/app/_services/sound.service.ts
+++ src/app/_services/sound.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AssetType } from 'asset';
+import { MapService } from 'services/map.service';
 import { AssetTypeToClips } from 'sound';
 
 import * as fs from 'fs';
@@ -10,7 +11,6 @@ export class SoundService {
   public soundMap: Map<string, Map<string, HTMLAudioElement>>;
 
   constructor() {
-    this.soundMap = new Map();
     this.nameToAudio = new Map();
   }
 
@@ -18,13 +18,14 @@ export class SoundService {
  * Reads the SoundClips dat file and populates the map's soundMap.
  */
   public parseSndData() {
+    this.soundMap = new Map();
     const sndData = this.readSndDat().trim();
     const [, sampleRate, songCount, songs, clipCount, clips] = sndData.split(/#.*?\r?\n/);
     const lines = clips.split(/\r?\n/);
 
     for (let i = 0; i < lines.length; i += 2) {
       const [, type, file] = lines[i + 1].split('/');
-      const filepath = '../dist/assets/snd/' + type + '/' + file;
+      const filepath = '../data/snd/' + type + '/' + file;
       const checkedPath = this.checkForCustomSound(filepath);
       const checkedAudio = new Audio(checkedPath);
       if (this.soundMap.has(type)) {
@@ -42,7 +43,7 @@ export class SoundService {
   public readSndDat(): string {
     let content: string;
 
-    content = fs.readFileSync('src/assets/snd/SoundClips.dat', 'utf8');
+    content = fs.readFileSync('data/snd/SoundClips.dat', 'utf8');
 
     if (content === undefined) {
       throw new Error('File not read');
@@ -53,11 +54,11 @@ export class SoundService {
 
   public checkForCustomSound(filepath: string): string {
     const [, , , type, file] = filepath.split('/');
-    const customFilePath = 'src/assets/customSnd/' + type + '/' + file;
-
+    const customFilePath = 'dist/../data/customSnd/' + type + '/' + file;
     try {
       fs.accessSync(customFilePath);
-      return customFilePath;
+      console.log(customFilePath);
+      return '../' + customFilePath;
     } catch (e) {
       return filepath;
     }
@@ -66,8 +67,6 @@ export class SoundService {
   // stolen from https://stackoverflow.com/questions/38595524/copy-a-source-file-to-another-destination-in-nodejs
   public copyFile(src, dest, category, sound, clip) {
     const readStream = fs.createReadStream(src);
-    console.log('src: ' + src);
-    console.log('dest: ' + dest);
 
     readStream.once('error', (err) => {
       console.log(err);
@@ -76,12 +75,10 @@ export class SoundService {
     readStream.pipe(fs.createWriteStream(dest)).on('finish', () => {
       this.editSoundMap(category, sound, clip);
       // document.getElementById('soundplayers').innerHTML = '<audio id="audio-player" controls="controls" src="../' + clip.src + '" type="audio/wav">';
-      console.log('done copying');
     });
   }
 
   public editSoundMap(category, sound, clip) {
-    console.log('editSound');
     this.soundMap.get(category).set(sound, clip);
     const newsnd = this.soundMap.get(category).get(sound);
     document.getElementById('soundplayers').innerHTML = '<audio id="audio-player" controls="controls" src="' + newsnd.src + '" type="audio/wav">';
--- src/app/audio/audio.component.ts
+++ src/app/audio/audio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Menu } from 'electron';
+import { MapService } from 'services/map.service';
 import { SerializeService } from 'services/serialize.service';
 import { SoundService } from 'services/sound.service';
 const { dialog } = require('electron').remote;
@@ -29,6 +30,7 @@ export class AudioComponent implements OnInit {
 
   constructor(
     private soundService: SoundService,
+    private mapService: MapService,
   ) {
   }
 
@@ -36,19 +38,26 @@ export class AudioComponent implements OnInit {
     this.soundService.parseSndData();
     this.SongCategories = [...this.soundService.soundMap.keys()];
     this.selectedCategory = this.SongCategories[0];
+
+    this.mapService.mapProjectLoaded.do(() => console.log('soundLoaded')).subscribe({
+      next: async () => {
+        this.soundService.parseSndData();
+        this.SongCategories = [...this.soundService.soundMap.keys()];
+        this.selectedCategory = this.SongCategories[0];
+      },
+      error: err => console.error(err),
+      complete: null
+    });
+
   }
 
 
   showSound(category) {
     this.isSoundLoaded = true;
-    this.Sounds = [];
-    const clipNames = this.soundService.soundMap.get(category).keys();
-    for (const name of clipNames) {
-      this.Sounds.push(name);
-    }
     this.selectedCategory = category;
-    // document.getElementById('soundplayers').innerHTML = '<audio id="audio-player" controls="controls" src="' + this.soundService.soundMap.get(item) + '" type="audio/wav">';
 
+    this.Sounds = [...this.soundService.soundMap.get(category).keys()];
+    // document.getElementById('soundplayers').innerHTML = '<audio id="audio-player" controls="controls" src="' + this.soundService.soundMap.get(item) + '" type="audio/wav">';
   }
 
   playSound(clipName) {
@@ -56,7 +65,7 @@ export class AudioComponent implements OnInit {
     this.selectedClip = this.soundService.soundMap.get(this.selectedCategory).get(clipName);
     const split = this.selectedClip.src.split('/');
     const file = split[split.length - 1];
-    this.destPath = '../dist/assets/customSnd/' + this.selectedCategory + '/' + file;
+    this.destPath = '../data/customSnd/' + this.selectedCategory + '/' + file;
     document.getElementById('soundplayers').innerHTML = '<audio id="audio-player" controls="controls" src="' + this.selectedClip.src + '" type="audio/wav">';
   }
 
@@ -64,16 +73,15 @@ export class AudioComponent implements OnInit {
     dialog.showOpenDialog(options, (paths: string[]) => {
       if (paths === undefined) return;
       const newClip = new Audio(paths[0]);
-      this.soundService.copyFile(paths[0], 'dist/' + this.destPath, this.selectedCategory, this.selectedClipName, newClip);
+      this.soundService.copyFile(paths[0], 'data/' + this.destPath, this.selectedCategory, this.selectedClipName, newClip);
     });
     console.log(this.soundService.soundMap);
   }
 
   revertAudio() {
-    console.log('DEST PATH; ' + this.destPath);
-    this.soundService.deleteSound('dist/' + this.destPath);
+    this.soundService.deleteSound('data/' + this.destPath);
     const name = this.destPath.split('customSnd')[1];
-    const orig = new Audio('../dist/assets/snd' + name);
+    const orig = new Audio('../data/snd' + name);
     this.soundService.editSoundMap(this.selectedCategory, this.selectedClipName, orig);
   }
 
